7cf23239d0e386af4feaef6e7b298ac0f3754ab7
Adán Lobato <adan.lobato@gmail.com>
2013-03-20 12:23:32 +0100

    Initial commit

---
+++ webhook.php
@@ -0,0 +1,59 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Yaml\Yaml;
+
+if (!file_exists(__DIR__.'/config.yml')) {
+    echo "Please, define your satis configuration in a config.yml file.\nYou can use the config.yml.dist as a template.";
+    exit(-1);
+}
+
+$defaults = array(
+    'bin' => 'bin/satis',
+    'json' => 'satis.json',
+    'webroot' => 'web/',
+    'user' => null,
+);
+$config = Yaml::parse(__DIR__.'/config.yml');
+$config = array_merge($defaults, $config);
+
+$errors = array();
+if (!file_exists($config['bin'])) {
+    $errors[] = 'The Satis bin could not be found.';
+}
+
+if (!file_exists($config['json'])) {
+    $errors[] = 'The satis.json file could not be found.';
+}
+
+if (!file_exists($config['webroot'])) {
+    $errors[] = 'The webroot directory could not be found.';
+}
+
+if (!is_writable($config['webroot'])) {
+    $errors[] = 'The webroot directory is not writable.';
+}
+
+if (!empty($errors)) {
+    echo 'The build cannot be run due to some errors. Please, review them and check your config.yml:'."\n";
+    foreach ($errors as $error) {
+        echo '- '.$error."\n";
+    }
+}
+
+$command = sprintf('%s build %s %s', $config['bin'], $config['json'], $config['webroot']);
+if (null !== $config['user']) {
+    $command = sprintf('sudo -u %s -i %s', $config['user'], $command);
+}
+
+$process = new Process($command);
+$exitCode = $process->run(function ($type, $buffer) {
+    if ('err' === $type) {
+        echo 'E';
+    } else {
+        echo '.';
+    }
+});
+
+echo "\n\n" . ($exitCode === 0 ? 'Successful rebuild! ☺' : 'Oops! An error occured! ☹') . "\n";
